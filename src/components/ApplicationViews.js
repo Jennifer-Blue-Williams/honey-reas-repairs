9a6c68f77ac0e1f15af30e324c7e06cc68936281
Jennifer-Blue-Williams <jennifer.blue.williams@gmail.com>
2022-01-25 22:27:15 -0600

    Service tickets rendering correctly

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Route } from "react-router-dom";
 import { CustomerList } from "./customers/CustomersList";
 import { EmployeeList } from "./employees/EmployeeList";
+import { TicketForm } from "./serviceTickets/TicketForm";
 import { TicketList } from "./serviceTickets/TicketList";
 
 export const ApplicationViews = () => {
@@ -15,9 +16,13 @@ export const ApplicationViews = () => {
                 <EmployeeList />
             </Route>
 
-            <Route path="/tickets">
+            <Route exact path="/tickets">
                 <TicketList />
             </Route>  
+
+            <Route path="/tickets/create">
+                <TicketForm />
+            </Route>
         </>
     )
 }
